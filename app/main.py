65276a91f8e5c09c91beb15661a548a67e7ad7ba
Andres <afcadiz@uc.cl>
2017-12-07 23:20:21 -0300

    added stylesheet and html

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, flash, render_template
 from werkzeug.utils import secure_filename
 from cubicator import start
 import zipfile
@@ -7,6 +7,7 @@ import zipfile
 ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
 
 app = Flask(__name__)
+app.secret_key = '1298389172jiklaf83276'
 
 # TODO make this work with non root paths, they most likely have to be created by docker on build
 app.config['UPLOAD_FOLDER'] = './'
@@ -18,19 +19,19 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No subiste ningún archivo!')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            flash('No subiste ningún archivo!')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -56,15 +57,11 @@ def upload_file():
             #TODO Cleanup
             return redirect(url_for('uploaded_file',
                                     filename=filename_clean))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+        else:
+            flash('Archivo inválido, debes subir un .xlsx o .xls')
+            return redirect(request.url)
+
+    return render_template('index.html',error=None)
 
 
 @app.route('/optimized/<filename>')
