b9c7d0060db974986d07b7be99b60129898a5ee8
Andres <afcadiz@uc.cl>
2017-12-08 15:25:14 -0300

    added cleanup and deleted zip on after request callback. close #7

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory, flash, render_template
+from shutil import rmtree
+from flask import Flask, request, redirect, url_for, send_from_directory, flash, render_template, after_this_request
 from werkzeug.utils import secure_filename
 from cubicator import start
 import zipfile
@@ -41,13 +42,13 @@ def upload_file():
             file.save(path_file)
 
             # now we call the optimization start and save the results on the respective folders.
-            path_result = os.path.join(app.config['RESULTS_FOLDER'], "result_{}".format(filename_clean))
-            path_images = os.path.join(app.config['IMAGES_FOLDER'], "images_{}".format(filename_clean))
+            path_result = os.path.join(app.config['RESULTS_FOLDER'], "resultado_{}".format(filename_clean))
+            path_images = os.path.join(app.config['IMAGES_FOLDER'], "imagenes_{}".format(filename_clean))
             os.makedirs(path_images)
             start(path_file, path_result, path_images + "/")
 
             # After optimization is over we add the results to zipfile.
-            zip_path = os.path.join(app.config['ZIPS_FOLDER'], "zip_{}.zip".format(filename_clean))
+            zip_path = os.path.join(app.config['ZIPS_FOLDER'], "cubicacion_{}.zip".format(filename_clean))
 
             with zipfile.ZipFile(zip_path, "w") as zip:
                 zip.write(path_result)
@@ -55,7 +56,14 @@ def upload_file():
                 for file in os.listdir(path_images):
                     zip.write(os.path.join(path_images, file))
 
-            # TODO Cleanup
+            #we clean up intermediary files
+            os.remove(path_result)
+            os.remove(path_result + '.xls')
+            rmtree(path_images)
+            os.remove(path_file)
+
+
+
             return redirect(url_for('uploaded_file',
                                     filename=filename_clean))
         else:
@@ -67,8 +75,15 @@ def upload_file():
 
 @app.route('/optimized/<filename>')
 def uploaded_file(filename):
+
+    @after_this_request
+    def remove_zip(response):
+        os.remove(os.path.join(app.config['ZIPS_FOLDER'],
+                               "cubicacion_{}.zip".format(filename)))
+        return response
+
     return send_from_directory(app.config['ZIPS_FOLDER'],
-                               "zip {}.zip".format(filename))
+                               "cubicacion_{}.zip".format(filename))
 
 
 if __name__ == "__main__":
